0102fa201c467df70b119d2ab4962572d7341ad4
Seakibble <seakibble@gmail.com>
2025-03-04 16:47:39 -0500

    Finished class

--- scripts/notes.js
+++ scripts/notes.js
@@ -16,14 +16,24 @@ if (notesArray == null) {
     })
 }
 
+let noteId = localStorage.getItem('id')
+if (noteId == null) {
+    noteId = 0
+}
 
 $add.addEventListener('click', (event) => {
     event.preventDefault()
 
     let note = {
+        id: noteId,
         title: $form.elements.title.value,
         text: $form.elements.text.value
     }
+    noteId++
+    localStorage.setItem('id', noteId)
+    $form.elements.title.value = ''
+    $form.elements.text.value = ''
+    
     notesArray.push(note)
     
     displayNote(note)
@@ -31,13 +41,36 @@ $add.addEventListener('click', (event) => {
     localStorage.setItem("notes", JSON.stringify(notesArray))
 })
 
+$clear.addEventListener('click', (event) => {
+    event.preventDefault()
+    $notes.innerHTML = ''
+    notesArray = []
+    localStorage.clear()
+})
+
+$notes.addEventListener('click', (event) => {
+    let $btn = event.target
+    if ($btn.tagName == 'BUTTON') {
+        let id = $btn.dataset.id
+        let index = notesArray.findIndex((note) => {
+            return note.id == id
+        })
+        notesArray.splice(index, 1)
+        localStorage.setItem("notes", JSON.stringify(notesArray))
+
+        $btn.parentElement.remove()
+    }
+})
+
+
 function displayNote(note) {
     let $noteDiv = document.createElement('div')
     $noteDiv.classList.add('note')
 
     $noteDiv.innerHTML = `
         <h2>${note.title}</h2>
-        <p>${note.text}</p>`
+        <p>${note.text}</p>
+        <button data-id="${note.id}" >X</button>`
     
     $notes.append($noteDiv)
 }
