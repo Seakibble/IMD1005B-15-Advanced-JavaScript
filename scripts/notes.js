9fa2d55e8deec38cdbcc5ded9e781bda423600ed
Seakibble <seakibble@gmail.com>
2025-03-06 13:45:17 -0500

    Finished Second Class

--- scripts/notes.js
+++ scripts/notes.js
@@ -24,10 +24,26 @@ if (noteId == null) {
 $add.addEventListener('click', (event) => {
     event.preventDefault()
 
+    let now = new Date()
+    
+    let date = now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear()
+    let hours = now.getHours()
+    let minutes = now.getMinutes()
+
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    let time = hours + ":" + minutes
+    
+
     let note = {
         id: noteId,
         title: $form.elements.title.value,
-        text: $form.elements.text.value
+        text: $form.elements.text.value,
+        timestamp: time + " - " + date
     }
     noteId++
     localStorage.setItem('id', noteId)
@@ -62,14 +78,57 @@ $notes.addEventListener('click', (event) => {
     }
 })
 
+let timer = null
+$search.addEventListener('keydown', (event) => {
+    if (timer != null) {
+        clearTimeout(timer)
+    }
 
-function displayNote(note) {
+    timer = setTimeout(search, 500)
+})
+
+function search() {
+    let query = $search.value.toLowerCase()
+    
+    if (query == '') {
+        $notes.innerHTML = ''
+        notesArray.forEach(note => {
+            displayNote(note)
+        })
+        return
+    }
+
+    let matches = []
+    console.log('Test')
+    for (let i = 0; i < notesArray.length; i++) {
+        console.log('Test2')
+        if (notesArray[i].title.toLowerCase().includes(query)) {
+            matches.push(notesArray[i])
+        } else if (notesArray[i].text.toLowerCase().includes(query)) {
+            matches.push(notesArray[i])
+        }
+    }
+    console.log(matches)
+    
+    $notes.innerHTML = `<p class='results'>Results: ${matches.length}</p>`
+    matches.forEach(note => {
+        displayNote(note, true)
+    })
+}
+
+
+function displayNote(note, highlight = false) {
     let $noteDiv = document.createElement('div')
     $noteDiv.classList.add('note')
+
+    if (highlight) {
+        $noteDiv.classList.add('highlight')
+    }
 
     $noteDiv.innerHTML = `
         <h2>${note.title}</h2>
         <p>${note.text}</p>
+        <p class='time'>${note.timestamp}</p>
         <button data-id="${note.id}" >X</button>`
     
     $notes.append($noteDiv)
